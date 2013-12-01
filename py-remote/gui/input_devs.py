54818a21ad4bd2a7d97e265054318fb050c49c0a
Vasili Bulkin <bulkin15@yandex.ru>
2013-12-01 15:45:28 +0400

    Multiple joystick support

--- py-remote/gui/input_devs.py
+++ py-remote/gui/input_devs.py
@@ -1,12 +1,15 @@
 import pygame
 
 class AnalogStick(object):
-    def __init__(self, id = 0, axes = (0,1)):
+    def __init__(self, id = 0, axes = ((0, 0), (0, 1))):
         self.axes = axes
-        self.joystick = pygame.joystick.Joystick(id)
-        self.joystick.init()
+        pygame.joystick.init()
+        self.joysticks = [ pygame.joystick.Joystick(x) for x in
+                           range(pygame.joystick.get_count()) ]
+        for j in self.joysticks:
+            j.init()
 
     def get_pos(self):
-        x = self.joystick.get_axis(self.axes[0])
-        y = self.joystick.get_axis(self.axes[1])        
+        x = self.joysticks[self.axes[0][0]].get_axis(self.axes[0][1])
+        y = self.joysticks[self.axes[1][0]].get_axis(self.axes[1][1])        
         return (x, y)
--- py-remote/test.py
+++ py-remote/test.py
@@ -5,7 +5,7 @@ from simplemesh import SimpleMeshParser
 def main():
     print map(ord, SimpleMeshParser(14).make_send_data_cmd(4, 'test'))
     engine = renderer.Renderer('Servo Control')
-    analog_stick = input.AnalogStick()
+    analog_stick = input_devs.AnalogStick()
     engine.add_widget(twoaxiswidget.TwoAxisWidget(size = (200,200), 
                                                     pos_func = analog_stick.get_pos),
                         (10,10))
